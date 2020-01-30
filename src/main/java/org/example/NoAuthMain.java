4be265bef32cac80126f53289bcb51ad17600e2e
Lukas Brandl <lukas.brandl@dc-square.de>
2020-01-30 09:41:17 +0100

    initial commit

---
+++ src/main/java/org/example/NoAuthMain.java
@@ -0,0 +1,62 @@
+
+/*
+ * Copyright 2020 dc-square GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.example;
+
+import com.hivemq.extension.sdk.api.ExtensionMain;
+import com.hivemq.extension.sdk.api.annotations.NotNull;
+import com.hivemq.extension.sdk.api.auth.SimpleAuthenticator;
+import com.hivemq.extension.sdk.api.parameter.ExtensionStartInput;
+import com.hivemq.extension.sdk.api.parameter.ExtensionStartOutput;
+import com.hivemq.extension.sdk.api.parameter.ExtensionStopInput;
+import com.hivemq.extension.sdk.api.parameter.ExtensionStopOutput;
+import com.hivemq.extension.sdk.api.services.Services;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Lukas Brandl
+ */
+public class NoAuthMain implements ExtensionMain {
+
+    private static final @NotNull Logger log = LoggerFactory.getLogger(NoAuthMain.class);
+    public static final SimpleAuthenticator ALLOW_ALL_AUTHENTICATOR = new AllowAllAuthenticator();
+
+    @Override
+    public void extensionStart(final @NotNull ExtensionStartInput extensionStartInput, final @NotNull ExtensionStartOutput extensionStartOutput) {
+
+        try {
+            Services.securityRegistry().setAuthenticatorProvider(authenticatorProviderInput -> ALLOW_ALL_AUTHENTICATOR);
+            log.warn("\n#####################################################################################################" +
+                    "\n# This is an insecure deployment. Every MQTT client is fully authorized.                            #" +
+                    "\n# For production usage add an authentication extension and remove the no-auth extension.            #" +
+                    "\n# Authentication extensions can be found in the marketplace (https://www.hivemq.com/extensions/).   #" +
+                    "\n#####################################################################################################");
+
+
+        } catch (Exception e) {
+            log.error("Exception thrown at extension start: ", e);
+        }
+
+    }
+
+    @Override
+    public void extensionStop(final @NotNull ExtensionStopInput extensionStopInput, final @NotNull ExtensionStopOutput extensionStopOutput) {
+
+    }
+
+}
